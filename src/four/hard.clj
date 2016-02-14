84d4b2280dc819c2659f5f12159c17f3e80f602f
graypawn <choi.pawn@gmail.com>
2016-02-15 01:41:48 +0900

    94 Game of Life

--- src/four/hard.clj
+++ src/four/hard.clj
@@ -75,3 +75,37 @@
            (/ (*' q (+ q 1) %) 2))
         [a b (* a b)])))
 
+;; Game of Life
+
+(defn p94-1 [board]
+  (let [neighbours (fn [[x y]]
+                     (for [dx [-1 0 1]
+                           dy [-1 0 1]
+                           :when (not= 0 dx dy)]
+                       [(+ x dx) (+ y dy)]))
+        
+
+        step (fn [cells]
+               (set (for [[loc n] (frequencies (mapcat neighbours cells))
+                          :when (or (= n 3) (and (= n 2) (cells loc)))]
+                      loc)))
+
+        line (fn [width s]
+               (apply str (map #(if (s %) \# \space) (range 0 width))))
+
+        lines (fn [hight width cells]
+                (map #(->> (get (group-by first cells) % [])
+                           (map second)
+                           set
+                           (line width))
+                     (range 0 hight)))]
+    (->> (keep-indexed #(keep-indexed
+                         (fn [n x] (if (= x \#) [% n] nil)) %2)
+                       board)
+         (apply concat [])
+         set
+         step
+         ()
+         (lines (count board) (count (first board)))
+         )))
+
